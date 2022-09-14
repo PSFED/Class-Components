afbff25ded7e5b08772975f5085192bb3fc11003
Pavel Semenov <x.pavel.semenov.x@gmail.com>
2022-09-14 19:11:20 +0300

    The first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CustomerFilter from './components/CustomerFilter'
+
+function App() {
+  return (
+    <div>
+      <CustomerFilter />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CustomerFilter.js
@@ -0,0 +1,84 @@
+import { Fragment, useState, useEffect, Component } from 'react'
+import Customers from './Customers'
+import styles from './CustomerFilter.module.css'
+
+// class CustomerFilter extends Component {
+//   static contextType = CustomersContext;
+
+//   constructor() {
+//     super();
+//     this.state = {
+//       filteredCustomers: [],
+//       filter: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     // Отправить HTTP запрос...
+//     this.setState({
+//       filteredCustomers: this.context.customers,
+//     });
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.filter !== this.state.filter) {
+//       this.setState({
+//         filteredCustomers: this.context.customers.filter((customer) =>
+//           customer.name.includes(this.state.filter)
+//         ),
+//       });
+//     }
+//   }
+
+//   filterHandler(event) {
+//     this.setState({
+//       filter: event.target.value,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <Fragment>
+//         <div className={styles.filter}>
+//           <input type="search" onChange={this.filterHandler.bind(this)} />
+//         </div>
+//         <ErrorBoundary>
+//           <Customers customers={this.state.filteredCustomers} />
+//         </ErrorBoundary>
+//       </Fragment>
+//     );
+//   }
+// }
+
+const DUMMY_CUSTOMERS = [
+  { id: 'c1', name: 'Дмитрий' },
+  { id: 'c2', name: 'Михаил' },
+  { id: 'c3', name: 'Ирина' },
+]
+
+const CustomerFilter = () => {
+  const [filteredCustomers, setFilteredCustomers] = useState(DUMMY_CUSTOMERS)
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    setFilteredCustomers(
+      DUMMY_CUSTOMERS.filter((customer) => customer.name.includes(filter))
+    )
+  }, [filter])
+
+  const filterHandler = (event) => {
+    setFilter(event.target.value)
+  }
+
+  return (
+    <Fragment>
+      <div className={styles.filter}>
+        <input type="search" onChange={filterHandler} />
+      </div>
+
+      <Customers customers={filteredCustomers} />
+    </Fragment>
+  )
+}
+
+export default CustomerFilter
